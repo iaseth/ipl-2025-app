7caaea0a2de97a981a1b5eb46234e76dc76a7fec
Ankur Seth <iankurseth@gmail.com>
2025-04-25 16:08:08 +0530

    Added footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,7 @@ import { pointsTable } from "$lib/data";
 	<table class="table max-w-3xl mx-auto text-right">
 		<thead>
 			<tr>
+				<th>#</th>
 				<th class="text-left">Team</th>
 				<th>Matches</th>
 				<th>Won</th>
@@ -31,8 +32,9 @@ import { pointsTable } from "$lib/data";
 		</thead>
 
 		<tbody>
-			{#each pointsTable as team}
+			{#each pointsTable as team, index}
 				<tr>
+					<td>{index+1}</td>
 					<td class="text-left">{team.team}</td>
 					<td>{team.matches}</td>
 					<td>{team.wins}</td>
@@ -40,9 +42,22 @@ import { pointsTable } from "$lib/data";
 					<td>{team.points}</td>
 					<td>{team.nrr}</td>
 				</tr>
+
+				<tr hidden>
+					<td></td>
+					<td colspan="6" class="text-center"></td>
+				</tr>
 			{/each}
 		</tbody>
 	</table>
 </section>
+
+<footer class="h-32"></footer>
+
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content px-4 py-12">
+	<aside>
+		<p>Copyright © {new Date().getFullYear()} Ankur Seth</p>
+	</aside>
+</footer>
 
 <RainbowBorder />
