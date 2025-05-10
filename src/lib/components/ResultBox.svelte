5cb7453a076cd7188520b5ab45341a6da9e74c3a
Ankur Seth <iankurseth@gmail.com>
2025-05-10 08:11:42 +0530

    Minor changes

--- src/lib/components/ResultBox.svelte
+++ src/lib/components/ResultBox.svelte
@@ -13,11 +13,11 @@ const pc = positive * 100 / simulations;
 
 </script>
 
-<div class="py-1 space-y-1.5 text-sm">
-	<p class="font-bold opacity-70">{header}</p>
-	<h2 class="text-xl md:text-2xl lg:text-3xl font-bold">
+<div class="py-1 space-y-2 text-sm">
+	<p class="font-bold opacity-60">{header}</p>
+	<h2 class="text-xl md:text-2xl lg:text-3xl font-bold opacity-75">
 		<span>{pc < 10 ? pc.toFixed(2) : pc.toFixed(1)}</span>
 		<span class="text-xl">%</span>
 	</h2>
-	<p class="opacity-70">{footer}</p>
+	<p class="opacity-60">{footer}</p>
 </div>
--- src/lib/components/TeamRow.svelte
+++ src/lib/components/TeamRow.svelte
@@ -28,13 +28,13 @@ const { simulations } = result;
 	<td>{team.nrr}</td>
 </tr>
 
-<tr>
+<tr class="border-b-2 border-purple-500 last:border-b-0">
 	<td colspan="7" class="text-center">
 		<section class="grid grid-cols-4">
-			<ResultBox positive={result.top2} {simulations} header="Top 2" footer="Points" />
-			<ResultBox positive={result.top2nrr} {simulations} header="Top 2" footer="NRR" />
-			<ResultBox positive={result.top4} {simulations} header="Top 4" footer="Points" />
-			<ResultBox positive={result.top4nrr} {simulations} header="Top 4" footer="NRR" />
+			<ResultBox positive={result.top2} {simulations} header="Top 2" footer="on Points" />
+			<ResultBox positive={result.top2nrr} {simulations} header="Top 2" footer="on NRR" />
+			<ResultBox positive={result.top4} {simulations} header="Top 4" footer="on Points" />
+			<ResultBox positive={result.top4nrr} {simulations} header="Top 4" footer="on NRR" />
 		</section>
 	</td>
 </tr>
